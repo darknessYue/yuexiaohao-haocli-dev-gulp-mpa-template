3a81f4ecafa2698ce0355d5e1e3aac89327a5e64
hao yue <haoyue@publicissapient.com>
2023-12-21 11:56:54 +0800

    init

---
+++ template/CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [0.1.0] - 2021-11-04
+
+- Initial release
+
+<!-- http://keepachangelog.com/ -->
+
+<!-- [0.1.1]: https://github.com/darkness/gulp-mpa/compare/v0.1.0...v0.1.1 -->
+[0.1.0]: https://github.com/darkness/gulp-mpa/releases/tag/v0.1.0
---
+++ template/lib/index.js
@@ -0,0 +1,280 @@
+/*
+ ======================================================
+ * @author hao
+ * @time 202111
+ * MIT
+ * gulp 入口文件
+ * 文件操作API 和 插件使用
+ * 插件：
+ * gulp-clean-css 压缩css
+ * gulp-rename 重命名扩展名 rename({extname: '.min.css'})
+ * gulp-sass sass({outputStyle: 'expanded'})  样式编译
+ * gulp-babel(平台) @babel/core @babel/preset-env 脚本编译
+ * gulp-swig 模板编译
+ * gulp-imagemin 图片压缩 通过c++完成的模块  需要下载二进制的程序集(都在国外)
+ * del 文件清除
+ * gulp-load-plugins 自动加载插件
+ * browser-sync 自动构建插件 热更新开发服务器
+ * gulp-useref 通过注释完成打包过程的文件合并和使用别的引用路径
+ * gulp-htmlmin 压缩html
+ * gulp-uglify 压缩js
+ * gulp-clean-css 压缩css
+ * gulp-if 条件判断
+ * gulp-browserify 解析浏览器无法识别的require等api
+ * gulp-util 提示信息
+ * gulp-file-include 模板组合
+ * gulp-autoprefixer css前缀
+ ======================================================
+*/
+
+const del = require('del')
+const yargs = require('yargs')
+const Path = require('path')
+const webpack = require('webpack-stream')
+// const webpackConfig = require("./webpack.config.js")
+const named = require('vinyl-named')
+
+const { series, parallel, src, dest, watch } = require('gulp')
+const browsersync = require('browser-sync')
+const bs = browsersync.create()
+const cwd = process.cwd() // 会返回当前命令行的工作目录
+let config = {
+  data: {},
+  build: {
+    src: 'src',
+    dist: 'dist',
+    temp: 'temp',
+    public: 'public',
+    watch: {
+      styles: 'assets/css/**/*.scss'
+    },
+    paths: {
+      styles: 'assets/css/*.scss',
+      scripts: 'assets/js/*.js',
+      pages: '*.html',
+      images: 'assets/images/**',
+      fonts: 'assets/fonts/**'
+    }
+  }
+}
+
+// 环境判断
+const { argv } = yargs
+let isProd = true
+if (argv._[0] !== 'build') {
+  isProd = false
+}
+
+try {
+  const loadConfig = require(`${cwd}/pages.config.js`)
+  if (loadConfig.build) {
+    loadConfig.build = Object.assign({}, config.build, loadConfig.build)
+  }
+  config = Object.assign({}, config, loadConfig)
+} catch (error) {
+
+}
+
+const loadPlugins = require('gulp-load-plugins')
+const plugins = loadPlugins()
+const {
+  // babel,
+  util,
+  swig,
+  useref,
+  imagemin,
+  fileInclude,
+  autoprefixer
+} = plugins
+
+const watchs = require('gulp-watch')
+const scss = require('gulp-sass')(require('sass'))
+
+/*
+*  webpack配置
+*/
+const webpackConfig = {
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd ? false : 'eval-cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              // '@babel/plugin-proposal-object-rest-spread',
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  absoluteRuntime: false,
+                  corejs: 3,
+                  helpers: true,
+                  regenerator: true,
+                  useESModules: false
+                }
+              ]
+            ],
+            presets: ['@babel/preset-env']
+            // cacheDirectory: true,
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+
+    }
+  }
+}
+
+/*
+*  错误提示配置
+*/
+function report (origin = 'script', detail) {
+  util.log(`[${origin}]`, detail.toString({
+    modules: false,
+    errorDetails: false,
+    timings: false,
+    cached: false,
+    colors: true
+  }))
+}
+
+/*
+* 样式文件迁移
+*/
+const style = () => {
+  return src(config.build.paths.styles, { base: config.build.src, cwd: config.build.src })
+    .pipe(scss({ outputStyle: 'expanded' }))
+    .pipe(autoprefixer())
+  // .pipe(rename({extname: '.min.css'}))
+    .pipe(dest(isProd ? config.build.dist : config.build.temp))
+    .pipe(bs.reload({ stream: true }))
+}
+
+/*
+* JavaScript文件迁移
+*/
+const scripts = () => {
+  return src(config.build.paths.scripts, { base: config.build.src, cwd: config.build.src })
+    .pipe(named(function (file) {
+      return file.relative.slice(0, -Path.extname(file.path).length)
+    }))
+    .pipe(webpack(webpackConfig))
+    .pipe(dest(isProd ? config.build.dist : config.build.temp))
+    .pipe(bs.reload({ stream: true }))
+}
+
+/*
+* 页面文件迁移
+*/
+const page = () => {
+  return src(config.build.paths.pages, { base: config.build.src, cwd: config.build.src })
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: `./${config.build.src}/`,
+      indent: true
+    }).on('error', (error) => {
+      report('template', error)
+    }))
+    .pipe(swig({ data: config.data, defaults: { cache: false } }))
+    .pipe(dest(config.build.temp))
+}
+
+/*
+* 图片文件迁移
+*/
+const image = () => {
+  return src(config.build.paths.images, { base: config.build.src, cwd: config.build.src })
+    .pipe(imagemin())
+    .pipe(dest(config.build.dist))
+}
+
+/*
+* 字体文件迁移
+*/
+const font = () => {
+  return src(config.build.paths.fonts, { base: config.build.src, cwd: config.build.src })
+    .pipe(imagemin())
+    .pipe(dest(config.build.dist))
+}
+
+/*
+* 公用文件迁移
+*/
+const extra = () => {
+  return src('**', { base: config.build.public, cwd: config.build.public })
+    .pipe(dest(config.build.dist))
+}
+
+/*
+* 清空文件夹
+*/
+const clean = () => {
+  return del([config.build.dist, config.build.temp], { force: true })
+}
+
+/*
+* 编译Html 修改其中的node_modules引用并打包
+*/
+const userefMethod = () => {
+  return src(`${config.build.temp}/${config.build.paths.pages}`, { base: config.build.temp })
+    .pipe(useref({ searchPath: [config.build.temp, '.'] })) // '.'表示当前目录
+    .pipe(plugins.if(/\.js$/, plugins.uglify()))
+    .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+    .pipe(plugins.if(/\.html$/, plugins.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    })))
+    .pipe(dest(config.build.dist))
+}
+
+/*
+* 静态服务器及文件监听
+*/
+const serve = () => {
+  watch(config.build.watch.styles, { events: 'all', cwd: config.build.src }, style)
+  watch(config.build.paths.scripts, { events: 'all', cwd: config.build.src }, scripts)
+  watchs(`${config.build.src}/**/${config.build.paths.pages}`, series(page, bs.reload))
+
+  watch([
+    config.build.paths.images,
+    config.build.paths.fonts,
+    `${config.build.public}/**`
+  ], { events: 'all', cwd: config.build.src }, series(image, font, extra, bs.reload))
+
+  bs.init({
+    // notify: false, // 提示
+    // port: 3080, // 端口
+    // open: false, // 是否自动打开浏览器
+    // files: 'dist/**', // 监听的文件路径
+    server: {
+      baseDir: [config.build.temp, config.build.src, config.build.public],
+      directory: true,
+      routes: {
+        '/node_modules': 'node_modules'
+      }
+    }
+  })
+}
+
+/*
+* 暴露命令
+*/
+const compile = series(style, scripts, page)
+exports.clean = clean
+exports.dev = series(compile, serve)
+exports.build = series(
+  clean,
+  parallel(
+    series(compile, userefMethod),
+    image,
+    font,
+    extra
+  )
+)
